26b55c1bb12fbd52a3bdb88d623925de42705aca
MatthewKlemesrud <matthew.klemesrud@ultima.com>
2017-02-03 10:03:50 +0000

    Updated forest and dc deployment script to include a transcript.

--- SCCMLab/DC Deployment/ConfigureForestDC.ps1
+++ SCCMLab/DC Deployment/ConfigureForestDC.ps1
@@ -1,10 +1,17 @@
-﻿Add-WindowsFeature -Name “RSAT-AD-Tools”
+﻿[CmdletBinding()]
+Param(
+  [Parameter(Mandatory=$false)]
+   [string]$DomainName='lab.ads',
+	
+   [Parameter(Mandatory=$false)]
+   [string]$NetBiosName='LAB'
+)
+Start-Transcript -Path "C:\WindowsAzure\Logs\Plugins\Microsoft.Compute.CustomScriptExtension\ConfigureForestDC.log"
+Add-WindowsFeature -Name “RSAT-AD-Tools”
 Add-WindowsFeature -Name “ad-domain-services” -IncludeAllSubFeature -IncludeManagementTools 
 Add-WindowsFeature -Name “dns” -IncludeAllSubFeature -IncludeManagementTools 
 Add-WindowsFeature -Name “gpmc” -IncludeAllSubFeature -IncludeManagementTools
 
-$domainname = “klem.ads”
-$netbiosName = “KLEM” 
 Import-Module ADDSDeployment
 Install-ADDSForest -CreateDnsDelegation:$false ` 
 -DatabasePath “C:\Windows\NTDS” ` 
@@ -16,4 +23,6 @@ Install-ADDSForest -CreateDnsDelegation:$false `
 -LogPath “C:\Windows\NTDS” ` 
 -NoRebootOnCompletion:$false ` 
 -SysvolPath “C:\Windows\SYSVOL” ` 
--Force:$true
+-Force:$true`
+-whatif
+Stop-Transcript
