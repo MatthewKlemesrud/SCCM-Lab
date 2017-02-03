31f67f5fc6c949553b94a3fd829662debfb6da6f
MatthewKlemesrud <matthew.klemesrud@ultima.com>
2017-02-03 10:58:18 +0000

    Addedd SMAdminPW Secure String

--- SCCMLab/DC Deployment/ConfigureForestDC.ps1
+++ SCCMLab/DC Deployment/ConfigureForestDC.ps1
@@ -19,14 +19,12 @@ Add-WindowsFeature -Name “gpmc” -IncludeAllSubFeature -IncludeManagementTool
 Import-Module ADDSDeployment
 Install-ADDSForest -CreateDnsDelegation:$false `
 -DatabasePath “C:\Windows\NTDS” `
--DomainMode “Win2012” `
 -DomainName $domainname `
 -DomainNetbiosName $netbiosName `
--ForestMode “Win2016” `
 -InstallDns:$true `
 -LogPath “C:\Windows\NTDS” `
 -NoRebootOnCompletion:$false `
 -SysvolPath “C:\Windows\SYSVOL” `
--Force:$true`
+-SafeModeAdministratorPassword $SafeModeAdminPW
 -whatif
 Stop-Transcript
