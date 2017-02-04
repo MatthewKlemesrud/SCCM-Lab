94d3378564bb521d33f26e84d5e456c7bd40115d
MatthewKlemesrud <matthew.klemesrud@ultima.com>
2017-02-04 15:14:16 +0000

    Removed password params

--- SCCMLab/DeploymentControl.ps1
+++ SCCMLab/DeploymentControl.ps1
@@ -5,23 +5,31 @@ Param(
    [Parameter(Mandatory=$True)]
    [string]$subscriptionId,
    
-   [Parameter(Mandatory=$True)]
-   [string]$resourceGroupName='SCCM-Lab',
+   [Parameter(Mandatory=$false)]
+   [string]$resourceGroupName = 'SCCM-Lab',
 
    [Parameter(Mandatory=$True)]
    [string]$resourceGroupLocation,
 
+   [Parameter(Mandatory=$false)]
+   [string]$Path=$PSScriptRoot + '\',
+
 #Iaas Deployment Parameters
 
-   [Parameter(Mandatory=$True)]
-   [string]$deploymentName='SCCM-Lab',
+   [Parameter(Mandatory=$false)]
+   [string]$deploymentName = 'SCCM-Lab',
 
-   [Parameter(Mandatory=$True)]
-   [string]$templateFilePath='C:\SCCM Lab\template.json',
+   [Parameter(Mandatory=$false)]
+   [string]$templateFileName='template.json',
 
-   [Parameter(Mandatory=$True)]
-   [string]$parametersFilePath='C:\SCCM Lab\parameters.json',
+   [Parameter(Mandatory=$false)]
+   [string]$parametersFileName='parameters.json',
 
+   [Parameter(Mandatory=$false)]
+   [string]$templateFilePath=$Path + $templateFileName,
+
+   [Parameter(Mandatory=$false)]
+   [string]$parametersFilePath=$Path + $parametersFileName,
 
 #Domain Controller Parameters
 
@@ -31,24 +39,42 @@ Param(
    [Parameter(Mandatory=$false)]
    [string]$NetBiosName='LAB',
 
-   [Parameter(Mandatory=$true)]
-   $SafeModeAdminPW= (Read-Host -Prompt "Enter the SafeMode Administrator password for the domain controller")
+   [Parameter(Mandatory=$false)]
+   $SecureSafeModeAdminPW=(Read-Host -AsSecureString -Prompt "Enter the SafeMode Administrator password for the domain controller")
 )
 
-$subscriptionId
-$resourceGroupName
+write-host "#################  ARM Template Deployment Settings ################"
+write-host "Subscription ID Has been set to        : " -nonewline
+write-host $subscriptionId -ForegroundColor:Green
+write-host "Resource Group name has been set to    : " -nonewline
+write-host $resourceGroupName -ForegroundColor:Green
+write-host "Resource Group location has been set to: " -nonewline
+write-host $resourceGroupLocation -ForegroundColor:Green
+write-host "Path to template files has been set to : " -nonewline
+write-host $Path -ForegroundColor:Green
+write-host "Template file name has been set to     : " -nonewline
+write-host $templateFileName -ForegroundColor:Green
+write-host "Parameters file name has been set to   : " -nonewline
+write-host $parametersFileName -ForegroundColor:Green
+write-host "The deployment name has been set to    : " -nonewline
+write-host $deploymentName -ForegroundColor:Green
+
+Write-Host "The Deploy-AzureResourceGroup.pst script will be run with the above parameters, are you sure you wish to continue?" -ForegroundColor:Red
 Start-Sleep
 #Deploy IaaS
 
-.\Deploy-AzureResourceGroup.ps1 -subscriptionId $subscriptionId -resourceGroupName $resourceGroupName -resourceGroupLocation $resourceGroupLocation -deploymentName $deploymentName -templateFilePath $templateFilePath -parametersFilePath 'C:\Temp\SCCM Lab\parameters.json'
 
-$SecureSafeModeAdminPW = ConvertTo-SecureString -String $SafeModeAdminPW -force
-Set-AzureKeyVaultSecret -VaultName 'Password-Safe' -Name 'DCSafeModeAdminPW' -SecretValue $SafeModeAdminPW -ContentType -WhatIf
+&$path'Deploy-AzureResourceGroup.ps1' -subscriptionId $subscriptionId -resourceGroupName $resourceGroupName -resourceGroupLocation $resourceGroupLocation -deploymentName $deploymentName -templateFilePath $templateFilePath -parametersFilePath 'C:\Temp\SCCM Lab\parameters.json'
 
+
+######   Domain Controller Deployment    ####################################
 #Deploy Domain Controller Script
+$BSTR = [System.Runtime.InteropServices.Marshal]::SecureStringToBSTR($SecureSafeModeAdminPW)            
+$SafeModeAdminPW = [System.Runtime.InteropServices.Marshal]::PtrToStringAuto($BSTR)            
+
 Login-AzureRmAccount -SubscriptionId $subscriptionId
 Select-AzureRmSubscription -SubscriptionId $subscriptionId
-Set-AzureRmVMCustomScriptExtension -VMName "TestDC" -ResourceGroupName $resourceGroupName -Name "Deploy_Forest_and_Domain_Controller" -FileUri "https://raw.githubusercontent.com/MatthewKlemesrud/SCCM-Lab/master/SCCMLab/DC%20Deployment/ConfigureForestDC.ps1" -Run "ConfigureForestDC.ps1 -SafeModeAdminPW $SafeModeAdminPW" -Location "West Europe" -ForceRerun:$true
+Set-AzureRmVMCustomScriptExtension -VMName "CM-LAB-DC" -ResourceGroupName $resourceGroupName -Name "Deploy_Forest_and_Domain_Controller" -FileUri "https://raw.githubusercontent.com/MatthewKlemesrud/SCCM-Lab/master/SCCMLab/DC%20Deployment/ConfigureForestDC.ps1" -Run "ConfigureForestDC.ps1 -SafeModeAdminPW $SafeModeAdminPW" -Location "West Europe" -ForceRerun:$true
 
 
 ############################################################################
